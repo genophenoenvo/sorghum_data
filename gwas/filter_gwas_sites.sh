a76d59adf60ec4543c930497dfbcb9dcf363acd8
Kevin Schaper <kevinschaper@gmail.com>
2022-10-26 10:54:32 -0700

    Updated gwas filter output for max growth p < 0.01, matrix creation notebook has tweaks to un-dask it and run subsets

--- gwas/filter_gwas_sites.sh
+++ gwas/filter_gwas_sites.sh
@@ -2,6 +2,6 @@
 
 for phenotype in max_growth_cm_gdd max_height_cm
 do
-  awk -F ',' 'BEGIN {OFS="\t"} (NR==1 || $8 < 0.01) {print $0}' $phenotype.FarmCPU.csv > $phenotype.FarmCPU.p001.csv
-  cat $phenotype.FarmCPU.p001.csv | cut -d, -f 2,3 | grep -v "CHROM" | sort | uniq | sed 's@"@@g' | tr ',' '\t' > $phenotype.FarmCPU.p001.unique.csv
+  awk -F ',' 'BEGIN {OFS="\t"} (NR==1 || $8 < 0.005) {print $0}' $phenotype.FarmCPU.csv > $phenotype.FarmCPU.p0005.csv
+  cat $phenotype.FarmCPU.p0005.csv | cut -d, -f 2,3 | grep -v "CHROM" | sort | uniq | sed 's@"@@g' | tr ',' '\t' > $phenotype.FarmCPU.p001.unique.csv
 done
